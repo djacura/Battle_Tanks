dfe471041fbf6273ad040cb4748d5184ae6e33ed
Daryl Jacura <d.jacura@sky.com>
2021-07-04 10:07:54 +0100

    Started Project in Unreal, then staged and commited changes to Git.

---
+++ Battle_Tanks/Source/Battle_Tanks/Battle_Tanks.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Battle_Tanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Battle_Tanks, "Battle_Tanks" );
---
+++ Battle_Tanks/Source/Battle_TanksEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Battle_TanksEditorTarget : TargetRules
+{
+	public Battle_TanksEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "Battle_Tanks" } );
+	}
+}
